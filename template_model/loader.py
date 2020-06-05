88bc06d55b5ecd37dd34cea6297283990c8a345d
J-E Castagnede <j.e.castagnede@gmail.com>
2020-06-05 16:58:11 +0200

    fix template reading

--- template_model/loader.py
+++ template_model/loader.py
@@ -29,7 +29,7 @@ class Loader(BaseLoader):
     def _load__template(self, template_name):
         template = Template.objects.get(template_file=template_name)
         with template.template_file.open('rb+') as my_template:
-            return my_template.read()
+            return my_template.read().decode()
 
     def _load_template_source(self, template_name, template_dirs=None):
         try:
