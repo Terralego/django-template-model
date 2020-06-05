c00c6e4cc72dc8b56a63e2d1e4d7f54c8828e11a
J-E Castagnede <j.e.castagnede@gmail.com>
2020-06-05 14:50:49 +0200

    add loader to handle database

---
+++ template_model/loader.py
@@ -0,0 +1,41 @@
+from django.contrib.sites.models import Site
+from django.db import router
+from django.template import Origin, TemplateDoesNotExist
+from django.template.loaders.base import Loader as BaseLoader
+
+from template_model.models import Template
+from template_model.utils.cache import (cache, get_cache_key,
+                                        set_and_return, get_cache_notfound_key)
+
+
+class Loader(BaseLoader):
+    """
+    A custom template loader to load templates from the database.
+
+    Tries to load the template from the dbtemplates cache backend specified
+    by the DBTEMPLATES_CACHE_BACKEND setting. If it does not find a template
+    it falls back to query the database field ``name`` with the template path
+    and ``sites`` with the current site.
+    """
+    is_usable = True
+
+    def get_template_sources(self, template_name, template_dirs=None):
+        yield Origin(
+            name=template_name,
+            template_name=template_name,
+            loader=self,
+        )
+
+    def get_contents(self, origin):
+        return self._load_template_source(origin.template_name)
+
+    def _load__template(self, template_name, cache_key, site, **params):
+        template = Template.objects.get(template_file=template_name, **params)
+        with template.template_file.open('rb+') as my_template:
+            return my_template.read()
+
+    def _load_template_source(self, template_name, template_dirs=None):
+        try:
+            return self._load__template(template_name)
+        except Template.DoesNotExist:
+            TemplateDoesNotExist(template_name)
